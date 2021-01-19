9682e6ef1d1b541e87a3722cd7d22e70a6a31eec
Christian Lamprecht <christian.lamprecht@aol.de>
2021-01-19 15:23:27 +0100

    Update __init__.py

--- meteostat/__init__.py
+++ meteostat/__init__.py
@@ -12,7 +12,7 @@ The code is licensed under the MIT license.
 """
 
 __appname__ = 'meteostat'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from .core import Core
 from .stations import Stations
