4192be15ee9b221887a5e1053701dd9f352278d5
clampr <christian.lamprecht@aol.de>
2021-02-02 16:41:08 +0100

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md')) as f:
 # Setup
 setup(
     name='meteostat',
-    version='1.1.0',
+    version='1.1.1',
     author='Meteostat',
     author_email='info@meteostat.net',
     description='Access and analyze historical weather and climate data with Python.',
