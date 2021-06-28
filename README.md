7cc44592eb0c1014f1cd24a8ff5978a14993fa6e
Christian Lamprecht <christian.lamprecht@aol.de>
2021-06-29 00:08:59 +0200

    Meteostat 1.5.1 (#54)
    * Meteostat 1.5.1
    
    * Update README
    
    * Update README

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ The Meteostat Python library provides a simple API for accessing open weather an
 
 Among the data sources are national weather services like the National Oceanic and Atmospheric Administration (NOAA) and Germany's national meteorological service (DWD).
 
+Are you looking for a **hosted solution**? Try our [JSON API](https://rapidapi.com/meteostat/api/meteostat/).
+
 ## Installation
 
 The Meteostat Python package is available through [PyPI](https://pypi.org/project/meteostat/):
--- examples/daily/compare.py
+++ examples/daily/compare.py
@@ -10,22 +10,14 @@ The code is licensed under the MIT license.
 
 from datetime import datetime
 import matplotlib.pyplot as plt
-from meteostat import Stations, Daily
-
-# Get weather stations by WMO ID
-stations = Stations()
-stations = stations.id('wmo', ('71624', '72295', '68816', '94767'))
-stations = stations.fetch()
-
-# Get names of weather stations
-names = stations['name'].to_list()
+from meteostat import Daily
 
 # Time period
 start = datetime(2019, 1, 1)
 end = datetime(2019, 12, 31)
 
 # Get daily data
-data = Daily(stations, start, end)
+data = Daily(['71624', '72295', '68816', '94767'], start, end)
 data = data.fetch()
 
 # Plot chart
@@ -33,5 +25,4 @@ data.unstack('station')['tavg'].plot(
     legend=True,
     ylabel='Avg. Daily Temperature °C',
     title='Average Temperature Report for 2019')
-plt.legend(names)
 plt.show()
