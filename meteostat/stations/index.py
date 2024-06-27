e55df725a37326f4ee9dd3e5b5107759570ab475
clampr <christian.lamprecht@aol.de>
2024-06-27 16:59:13 +0200

    Sort index on stations.index

--- meteostat/stations/index.py
+++ meteostat/stations/index.py
@@ -3,11 +3,12 @@ from requests import HTTPError, Timeout
 from meteostat import settings
 from meteostat.utils.decorators import cache
 
+
 @cache(60 * 60 * 24, "pickle")
 def index() -> pd.DataFrame | None:
     for mirror in settings.location_mirrors:
         try:
-            return pd.read_csv(mirror, compression="gzip", index_col="id")
+            return pd.read_csv(mirror, compression="gzip", index_col="id").sort_index()
         except (HTTPError, Timeout):
             continue
     return None
