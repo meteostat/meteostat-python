14b5bca2c210ce59026d4aaaebaa3ab47f77e4be
clampr <christian.lamprecht@aol.de>
2025-04-30 09:55:55 +0200

    fix: formatting

--- meteostat/series/normalize.py
+++ meteostat/series/normalize.py
@@ -28,7 +28,7 @@ def normalize(self):
 
     if temp._start and temp._end and temp.coverage() < 1:
         # Create result DataFrame
-        result = pd.DataFrame(columns=temp._processed_columns, dtype='Float64')
+        result = pd.DataFrame(columns=temp._processed_columns, dtype="Float64")
 
         # Handle tz-aware date ranges
         if hasattr(temp, "_timezone") and temp._timezone is not None:
@@ -42,7 +42,7 @@ def normalize(self):
         # Go through list of weather stations
         for station in temp._stations:
             # Create data frame
-            df = pd.DataFrame(columns=temp._processed_columns, dtype='Float64')
+            df = pd.DataFrame(columns=temp._processed_columns, dtype="Float64")
             # Add time series
             df["time"] = pd.date_range(
                 start,
